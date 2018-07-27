a49ff6110e5125a7e07a8fb2a056111e7eea8856
gorantladivya19 <41732058+gorantladivya19@users.noreply.github.com>
2018-07-27 11:18:29 +0530

    Create consonant.java

---
+++ consonant.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 Scanner s=new Scanner(System.in);
+	  char ch = s.next().charAt(0);
+	 if(ch == 'a' || ch == 'e' || ch=='i' || ch == 'o' || ch == 'u')
+	     System.out.println(ch + " is vowel");
+	 else
+	     System.out.println("invalid");
+	}
+}
